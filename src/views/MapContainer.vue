0af8ba7ddb2a49c551e051e43d4a9fc83843ed44
zheyi420 <37471153+zheyi420@users.noreply.github.com>
2024-10-25 01:02:00 +0800

    引入地图容器

---
+++ src/views/MapContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="mapContainer" class="map-container"></div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import ViewerManager from '@/map/ViewerManager';
+
+const mapContainer = ref(null);
+const viewerManager = new ViewerManager();
+
+onMounted(() => {
+  window.viewer = viewerManager.createViewer(mapContainer.value, {
+    // TODO Add your viewer options here
+  });
+});
+</script>
+
+<style scoped>
+.map-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
