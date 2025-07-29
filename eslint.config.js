c0815d3f07eb7f2a953e5122d7623ab91a3f9ba7
zheyi420 <37471153+zheyi420@users.noreply.github.com>
2025-07-30 00:06:38 +0800

    🐳 chore: 配置更新

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import globals from 'globals'
 
 export default [
   {
@@ -10,13 +11,22 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/public/static/Cesium/**'],
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,    // 包含 window, document, console 等浏览器全局变量
+        ...globals.node,       // 包含 process, Buffer 等 Node.js 全局变量
+        ...globals.es2021,     // 包含现代 JavaScript 全局变量
+        // 项目特定的全局变量
+        Cesium: 'readonly',    // Cesium 库
+      },
+    },
     rules: {
       'no-unused-vars': 'warn',
       'vue/no-unused-vars': 'warn',
--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'cesium/Build/Cesium/Widgets/widgets.css'
 window.CESIUM_BASE_URL = `${import.meta.env.BASE_URL}static/Cesium/`
 window.Cesium = Cesium
 window.Cesium.Ion.defaultAccessToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzNmU5ZTZhOS1lOThmLTRlM2QtYjg2NS1iMGQ1Y2JiZGQyYzUiLCJpZCI6MTA4NDQ0LCJpYXQiOjE2NjM1Njc5OTR9.CuE8Bqn8X02o64kfjVHZUiUU1bKiNeqWYXoY7e5_BCc'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1ZmY2ZWNkYy04ZjI4LTQ1ZjQtYjRhYS0wZjYwMDdmNmUyZTUiLCJpZCI6MTA4NDQ0LCJpYXQiOjE3NTM4MDQyNzV9.9007CkJcf5kmK2odPZVGTrWExOwhOZy6mqs0GD_wo7A'
 
 const app = createApp(App)
 
