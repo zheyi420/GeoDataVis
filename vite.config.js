82fc56ae6df341803803611456c8176e8c2b607f
zheyi420 <37471153+zheyi420@users.noreply.github.com>
2025-08-26 20:50:37 +0800

    🐳 chore: 构建配置修改

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,48 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    ElementPlus({
-      // options
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: { api: 'modern-compiler' },
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // 根据构建模式设置不同的输出目录
+  let outDir = 'dist'
+  if (mode === 'production-github-pages') {
+    outDir = 'dist/github-pages'
+  } else if (mode === 'production-local') {
+    outDir = 'dist/local'
+  }
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      ElementPlus({
+        // options
+      }),
+    ],
+    /*
+     * https://vite.dev/guide/static-deploy.html#github-pages
+     */
+    base: env.VITE_BASE_URL || '/',
+    build: {
+      outDir: outDir
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: { api: 'modern-compiler' },
+      }
     }
   }
 })
