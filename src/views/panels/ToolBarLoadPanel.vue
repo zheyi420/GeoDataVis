da6f26ac55538e37a1a1ac05a3a49cbee67bd3e7
zheyi420 <37471153+zheyi420@users.noreply.github.com>
2026-03-19 23:02:12 +0800

    ✨ feat(ToolBarLoadPanel): 合并加载入口

--- src/views/panels/ToolBarLoadPanel.vue
+++ src/views/panels/ToolBarLoadPanel.vue
@@ -2,7 +2,7 @@
  * @Author: zheyi420
  * @Date: 2024-12-15 03:28:42
  * @LastEditors: zheyi420
- * @LastEditTime: 2026-03-08
+ * @LastEditTime: 2026-03-19
  * @FilePath: \GeoDataVis\src\views\panels\ToolBarLoadPanel.vue
  * @Description: 工具栏，用于加载数据的面板，包括加载数据服务、加载本地数据文件等
  *
@@ -10,39 +10,20 @@
 
 <template>
   <div class="toolbar-load-panel">
-    <!-- 加载服务 -->
     <el-dropdown
-      ref="ref4ServiceLoadTypeDropdown"
+      ref="ref4AddLoadTypeDropdown"
       trigger="hover"
-      @visible-change="handleVisibleChange4ServiceLoadTypeDropdown"
+      @visible-change="handleVisibleChange4AddLoadTypeDropdown"
     >
       <template v-slot:default>
-        <span class="item">服务</span>
+        <span class="item">添加</span>
       </template>
       <template v-slot:dropdown>
         <el-cascader-panel
-          ref="ref4ServiceLoadTypeCascaderPanel"
-          :options="options4ServiceLoadType"
-          :props="props4ServiceLoadTypeOnCascaderPanel"
-          @change="handleChange4ServiceLoadType"
-        />
-      </template>
-    </el-dropdown>
-    <!-- 加载文件 -->
-    <el-dropdown
-      ref="ref4FileLoadTypeDropdown"
-      trigger="hover"
-      @visible-change="handleVisibleChange4FileLoadTypeDropdown"
-    >
-      <template v-slot:default>
-        <span class="item">文件</span>
-      </template>
-      <template v-slot:dropdown>
-        <el-cascader-panel
-          ref="ref4FileLoadTypeCascaderPanel"
-          :options="options4FileLoadType"
-          :props="props4FileLoadTypeOnCascaderPanel"
-          @change="handleChange4FileLoadType"
+          ref="ref4AddLoadTypeCascaderPanel"
+          :options="options4AddLoadType"
+          :props="props4AddLoadTypeOnCascaderPanel"
+          @change="handleChange4AddLoadType"
         />
       </template>
     </el-dropdown>
@@ -54,134 +35,83 @@ import { ref } from 'vue';
 import { ElDropdown, ElCascaderPanel } from 'element-plus';
 import { usePanelStatusStore } from '@/stores/panelStatus';
 
-/** 加载服务 */
-const ref4ServiceLoadTypeDropdown = ref(null);
-const ref4ServiceLoadTypeCascaderPanel = ref(null);
-const props4ServiceLoadTypeOnCascaderPanel = {
+const ref4AddLoadTypeDropdown = ref(null);
+const ref4AddLoadTypeCascaderPanel = ref(null);
+const props4AddLoadTypeOnCascaderPanel = {
   expandTrigger: 'hover',
 }
-const options4ServiceLoadType = [
+const options4AddLoadType = [
   {
     value: 'Web Map',
     label: 'Web Map',
     children: [
-      {
-        value: 'WMS',
-        label: 'WMS',
-      },
-      {
-        value: 'WMTS',
-        label: 'WMTS',
-      },
+      { value: 'WMS', label: 'WMS' },
+      { value: 'WMTS', label: 'WMTS' },
     ],
   },
   {
     value: '3D-Data',
     label: '3D-Data',
     children: [
-      {
-        value: 'Cesium3DTiles',
-        label: 'Cesium3DTiles',
-      }
+      { value: 'Cesium3DTiles', label: 'Cesium3DTiles' },
     ],
   },
   {
     value: 'DEM',
     label: 'DEM',
     children: [
-      {
-        value: 'CesiumTerrain',
-        label: 'Cesium Terrain',
-      },
+      { value: 'CesiumTerrain', label: 'Cesium Terrain' },
     ],
   },
+  { value: 'GeoJSON', label: 'GeoJSON' },
+  { value: 'KML', label: 'KML/KMZ' },
 ]
 
-function handleVisibleChange4ServiceLoadTypeDropdown(visible) {
-  // console.log('###服务“下拉列表visible-change', visible);
-
-  // console.log('ref4ServiceLoadTypeDropdown.value', ref4ServiceLoadTypeDropdown.value);
-  // console.log('ref4ServiceLoadTypeCascaderPanel.value', ref4ServiceLoadTypeCascaderPanel.value);
-
-  if (visible) {
-
-    // ref4ServiceLoadTypeDropdown.value.$el.classList.add('is-hover');
-  } else {
-    // 关闭下拉菜单时，清空展开节点记录
-    ref4ServiceLoadTypeCascaderPanel.value.clearCheckedNodes();
-    // ref4ServiceLoadTypeDropdown.value.$el.classList.remove('is-hover');
-    // ref4ServiceLoadTypeCascaderPanel.value.$el.blur();
-    // ref4ServiceLoadTypeDropdown.value.$el.blur();
-    /* setTimeout(() => {
-      ref4ServiceLoadTypeDropdown.value.$el.blur();
-    }, 0); */
+function handleVisibleChange4AddLoadTypeDropdown(visible) {
+  if (!visible) {
+    ref4AddLoadTypeCascaderPanel.value.clearCheckedNodes();
   }
 }
-function handleChange4ServiceLoadType(value) {
-  console.log('###handleChange4ServiceLoadType value', value, typeof value, Array.isArray(value));
 
-  // 关闭下拉菜单
-  ref4ServiceLoadTypeDropdown.value.handleClose();
-  // 清空选中的节点
-  ref4ServiceLoadTypeCascaderPanel.value.clearCheckedNodes();
+function handleChange4AddLoadType(value) {
+  ref4AddLoadTypeDropdown.value.handleClose();
+  ref4AddLoadTypeCascaderPanel.value.clearCheckedNodes();
 
+  if (!value || value.length === 0) return;
 
-  if (!value) return;
-
-  // 打开对应的加载服务面板
-  switch (value.length) {
-    case 1: {
-      console.log('###加载服务', value[0]);
-      break;
+  if (value.length === 2) {
+    switch (value[0]) {
+      case 'Web Map':
+        _loadGeoServerService(value[1]);
+        break;
+      case '3D-Data':
+        _load3DDataService(value[1]);
+        break;
+      case 'DEM':
+        _loadDemService(value[1]);
+        break;
+      default:
+        break;
     }
-    case 2: {
-      console.log('###加载服务', value[0], value[1]);
-      switch (value[0]) {
-        case 'Web Map': {
-          _loadGeoServerService(value[1]);
-          break;
-        }
-        case '3D-Data': {
-          _load3DDataService(value[1]);
-          break;
-        }
-        case 'DEM': {
-          _loadDemService(value[1]);
-          break;
-        }
-        default: {
-          console.log('###加载服务-未匹配2', value);
-          break;
-        }
-      }
-      break;
-    }
-    default: {
-      console.log('###加载服务-未匹配1', value);
-      break;
+  } else if (value.length === 1) {
+    if (value[0] === 'GeoJSON') {
+      usePanelStatusStore().openDialogGeoJsonParam();
+    } else if (value[0] === 'KML') {
+      usePanelStatusStore().openDialogKmlParam();
     }
   }
 }
 
 function _loadGeoServerService(value) {
-  console.log('###_loadGeoServerService value:', value);
-
-  // 打开GeoServer服务加载面板
   switch (value) {
-    case 'WMS': {
-      console.log('###加载服务-Web Map-WMS');
+    case 'WMS':
       usePanelStatusStore().openDialogWmsServiceParam();
       break;
-    }
-    case 'WMTS': {
-      console.log('###加载服务-Web Map-WMTS');
+    case 'WMTS':
       usePanelStatusStore().openDialogWmtsServiceParam();
       break;
-    }
-    default: {
-      console.log('###加载服务-Web Map-未匹配', value);
+    default:
       break;
-    }
   }
 }
 
@@ -224,54 +154,6 @@ function _loadDemService(value) {
     }
   }
 }
-
-/** 加载文件 */
-const ref4FileLoadTypeDropdown = ref(null);
-const ref4FileLoadTypeCascaderPanel = ref(null);
-const options4FileLoadType = [
-  {
-    value: 'GeoJSON',
-    label: 'GeoJSON',
-  },
-  {
-    value: 'KML',
-    label: 'KML/KMZ',
-  },
-]
-const props4FileLoadTypeOnCascaderPanel = {
-  expandTrigger: 'hover',
-}
-function handleVisibleChange4FileLoadTypeDropdown(visible) {
-  // console.log('###文件“下拉列表visible-change', visible);
-
-  if (visible) {
-    // console.log('ref4FileLoadTypeDropdown.value', ref4FileLoadTypeDropdown.value);
-
-    // ref4FileLoadTypeDropdown.value.classList.add('is-hover');
-  } else {
-    // 关闭下拉菜单时，清空展开节点记录
-    ref4FileLoadTypeCascaderPanel.value.clearCheckedNodes();
-    // ref4FileLoadTypeDropdown.value.classList.remove('is-hover');
-  }
-}
-function handleChange4FileLoadType(value) {
-  console.log('###handleChange4FileLoadType value', value);
-
-  // 关闭下拉菜单
-  ref4FileLoadTypeDropdown.value.handleClose();
-  // 清空选中的节点
-  ref4FileLoadTypeCascaderPanel.value.clearCheckedNodes();
-
-  if (!value || value.length === 0) return;
-
-  if (value[0] === 'GeoJSON') {
-    usePanelStatusStore().openDialogGeoJsonParam();
-  } else if (value[0] === 'KML') {
-    usePanelStatusStore().openDialogKmlParam();
-  }
-}
-
-
 </script>
 
 <style scoped lang="scss">
@@ -348,4 +230,3 @@ function handleChange4FileLoadType(value) {
   }
 }
 </style>
-
