e9405dc4647146ac0fbe4d42ac4cad133be47790
zheyi420 <37471153+zheyi420@users.noreply.github.com>
2026-04-10 23:42:52 +0800

    📃 docs(应用网页): 完善备案信息

--- src/views/panels/BeianInfo.vue
+++ src/views/panels/BeianInfo.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="beian-info">
-    <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
-      粤ICP备2026031216号-1
-    </a>
-    <span class="separator">|</span>
-    <span>公安备案审核中</span>
+    <img src="/备案图标.png" alt="备案图标" height="16" />
+    <a href="https://beian.mps.gov.cn/#/query/webSearch?code=44060402003172" rel="noreferrer" target="_blank">粤公网安备44060402003172号</a>
+    <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">粤ICP备2026031216号-1</a>
   </div>
 </template>
 
@@ -12,11 +10,12 @@
 .beian-info {
   display: flex;
   align-items: center;
-  gap: 8px;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 0.35rem 0.5rem;
   white-space: nowrap;
 
-  a,
-  span {
+  a {
     color: rgb(255, 255, 255);
     font-size: 12px;
     font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -26,12 +25,9 @@
     text-decoration: none;
 
     &:hover {
-      text-decoration: underline;
+      // text-decoration: underline;
+      color: #6dabe4;
     }
-  }
-
-  .separator {
-    opacity: 0.8;
   }
 }
 </style>
